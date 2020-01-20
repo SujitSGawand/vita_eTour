60f4afabb266a8963dbf03535843efa93a6fb53c
SujitSGawand <sujitsgawand@hotmail.com>
2020-01-20 09:51:27 +0530

    function written

--- Angular/IndiaTour/src/app/category-sub-category/category-sub-category.component.ts
+++ Angular/IndiaTour/src/app/category-sub-category/category-sub-category.component.ts
@@ -10,12 +10,28 @@ import { CategorySubCategoryService } from '../category-sub-category.service';
 export class CategorySubCategoryComponent implements OnInit {
 
   categorysubcategories:ICategorySubCategory[];
+  a:ICategorySubCategory[];
+ 
   constructor(private _catsubcatservice:CategorySubCategoryService) { }
-
-
+  icat1Disp(categorysubcategories:ICategorySubCategory[]) : ICategorySubCategory[]
+  {
+    let b:ICategorySubCategory[];
+    categorysubcategories.forEach(element => {
+      var i;
+      if(element.categoryname!=null)
+      {
+        for (i=0;i<=b.length;i++)
+          b[i] = element;
+      }
+      }    
+    );
+    return b;
+  }
+ 
   ngOnInit() {
-
+    
     this._catsubcatservice.GetCategorySubCategories().subscribe(data=>this.categorysubcategories=data) ;
+    this.a=this.icat1Disp(this.categorysubcategories);
   }
 
 }
